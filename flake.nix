9fe44ef9dec17bfb95b8c91b94a257e9413830ad
rumi <mailark@cock.li>
2026-03-25 10:15:41 +0530

    add nix-flatpak

--- flake.nix
+++ flake.nix
@@ -7,23 +7,28 @@
 
     home-manager = {
     	url = "github:nix-community/home-manager";
-	inputs.nixpkgs.follows = "nixpkgs";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-flatpak = {
+        url = "github:gmodena/nix-flatpak?ref=latest";
     };
   };
 
-  outputs = { nixpkgs, import-tree, home-manager, ... } @inputs: {
+  outputs = { nixpkgs, import-tree, home-manager, nix-flatpak ... } @inputs: {
     nixosConfigurations.domus = nixpkgs.lib.nixosSystem {
         modules = [
-            ./configuration.nix
-            (import-tree ./modules)
-	    home-manager.nixosModules.home-manager
-	    {
-		home-manager.useGlobalPkgs = true;
-		home-manager.useUserPackages = true;
-		home-manager.backupFileExtension = "bakup";
+                ./configuration.nix
+                nix-flatpak.nixosModules.nix-flatpak
+                (import-tree ./modules)
+                home-manager.nixosModules.home-manager
+                {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.backupFileExtension = "bakup";
 
-		home-manager.users.rumi = ./home.nix;
-	    }
+                    home-manager.users.rumi = ./home.nix;
+                }
         ];
     };
   };
