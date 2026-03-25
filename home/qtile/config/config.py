33599be20b3d16c1aecbb8b203799f2ccb50fae7
rumi <mailark@cock.li>
2026-03-25 09:58:24 +0530

    change focused border color

--- home/qtile/config/config.py
+++ home/qtile/config/config.py
@@ -111,7 +111,10 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(
+        border_focus="#d65d0e",
+        border_width=4
+        ),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
